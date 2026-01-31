85f163788b284e9b8bf97c194154b5b2240f5917
Lyubomir Doynov <floggy7x@gmail.com>
2026-01-31 16:52:47 +0200

    Add todos & snackbar props.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { SnackbarProvider } from 'notistack'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { SnackbarProvider } from "notistack";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SnackbarProvider>
+    <SnackbarProvider
+      maxSnack={3}
+      autoHideDuration={3000}
+    >
       <App />
     </SnackbarProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+//! TODO:
+/**
+ * Fix Visualizer on uptuning/downtuning. 
+ * 
+ * Speed up / speed down
+ * Song time on canvas hover
+ * Mute / Unmute
+ * Volume, Panner knobs
+ * compression, distortion, EQ
+ * use AI to remove certain instruments?
+ * 
+ */
